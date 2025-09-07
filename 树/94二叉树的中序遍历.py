48f4cba5c8849e25c586c90990edaaa110e110a5
120L021602 <18513366509@163.com>
2025-09-08 01:15:43 +0800

    add 94

---
+++ 树/94二叉树的中序遍历.py
@@ -0,0 +1,54 @@
+# 给定一个二叉树的根节点root ，返回它的中序遍历 。
+#
+#
+#
+# 示例
+# 1：
+#
+#
+# 输入：root = [1, null, 2, 3]
+# 输出：[1, 3, 2]
+# 示例
+# 2：
+#
+# 输入：root = []
+# 输出：[]
+# 示例
+# 3：
+#
+# 输入：root = [1]
+# 输出：[1]
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        if not root:
+            []
+
+        res = []
+        stack = deque()
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            node = stack.pop()
+            res.append(node.val)
+            cur = node.right
+
+        return res
+
