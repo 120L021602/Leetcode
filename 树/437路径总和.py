89fa4a677a875eec4c91438f72fe5f400b9c4823
120L021602 <18513366509@163.com>
2025-09-08 16:52:09 +0800

    add 437

---
+++ 树/437路径总和.py
@@ -0,0 +1,28 @@
+# 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+#
+# 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+from typing import Optional
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root:
+            return 0
+
+        def dfs(root, targetSum):
+            if not root:
+                return 0
+            count = 0
+            if root.val == targetSum:
+                count += 1
+            count += dfs(root.left, targetSum - root.val)
+            count += dfs(root.right, targetSum - root.val)
+            return count
+
+        return dfs(root, targetSum) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
