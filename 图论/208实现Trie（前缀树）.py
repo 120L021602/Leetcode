13fbcebc585ac11e64e1379915cafc77b8246933
120L021602 <18513366509@163.com>
2025-09-08 21:09:04 +0800

    update

--- 图论/208实现Trie（前缀树）.py
+++ 图论/208实现Trie（前缀树）.py
@@ -8,3 +8,43 @@
 # boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
 
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # 子节点，key 是字符，value 是 TrieNode
+        self.is_end = False  # 是否为单词结尾
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for s in word:
+            if s not in node.children:
+                node.children[s] = TrieNode()
+            node = node.children[s]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self._find_node(word)
+        return node is not None and node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self._find_node(prefix)
+        return node is not None
+
+    def _find_node(self, string: str) -> TrieNode:
+        node = self.root
+        for s in string:
+            if s not in node.children:
+                return None
+            node = node.children[s]
+        return node
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
