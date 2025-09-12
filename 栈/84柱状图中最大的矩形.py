e5ed96a1fdb03abc84c73faa596be773dc67c28a
120L021602 <18513366509@163.com>
2025-09-13 00:02:45 +0800

    add 384

---
+++ 栈/84柱状图中最大的矩形.py
@@ -0,0 +1,63 @@
+# 给定n个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为1 。
+#
+# 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+#
+#
+#
+# 示例
+# 1:
+#
+# 输入：heights = [2, 1, 5, 6, 2, 3]
+# 输出：10
+# 解释：最大的矩形为图中红色区域，面积为
+# 10
+# 示例
+# 2：
+#
+#
+#
+# 输入： heights = [2, 4]
+# 输出： 4
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        # Monotonic Stack
+        '''
+        找每个柱子左右侧的第一个高度值小于该柱子的柱子
+        单调栈：栈顶到栈底：从大到小（每插入一个新的小数值时，都要弹出先前的大数值）
+        栈顶，栈顶的下一个元素，即将入栈的元素：这三个元素组成了最大面积的高度和宽度
+        情况一：当前遍历的元素heights[i]大于栈顶元素的情况
+        情况二：当前遍历的元素heights[i]等于栈顶元素的情况
+        情况三：当前遍历的元素heights[i]小于栈顶元素的情况
+        '''
+
+        # 输入数组首尾各补上一个0（与42.接雨水不同的是，本题原首尾的两个柱子可以作为核心柱进行最大面积尝试
+        heights.insert(0, 0)
+        heights.append(0)
+        stack = [0]
+        result = 0
+        for i in range(1, len(heights)):
+            # 情况一
+            if heights[i] > heights[stack[-1]]:
+                stack.append(i)
+            # 情况二
+            elif heights[i] == heights[stack[-1]]:
+                stack.pop()
+                stack.append(i)
+            # 情况三
+            else:
+                # 抛出所有较高的柱子
+                while stack and heights[i] < heights[stack[-1]]:
+                    # 栈顶就是中间的柱子，主心骨
+                    mid_index = stack[-1]
+                    stack.pop()
+                    if stack:
+                        left_index = stack[-1]
+                        right_index = i
+                        width = right_index - left_index - 1
+                        height = heights[mid_index]
+                        result = max(result, width * height)
+                stack.append(i)
+        return result
