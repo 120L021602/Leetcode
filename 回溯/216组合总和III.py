e34b519ee104d6ef64fa892ab0d4f723204df210
120L021602 <18513366509@163.com>
2025-09-19 23:47:59 +0800

    add 216

---
+++ 回溯/216组合总和III.py
@@ -0,0 +1,62 @@
+# 找出所有相加之和为n的k个数的组合，且满足下列条件：
+#
+# 只使用数字1到9每个数字最多使用一次返回所有可能的有效组合的列表 。该列表不能包含相同的组合两次，组合可以以任何顺序返回。
+#
+#
+#
+# 示例
+# 1:
+#
+# 输入: k = 3, n = 7
+# 输出: [[1, 2, 4]]
+# 解释:
+# 1 + 2 + 4 = 7
+# 没有其他符合的组合了。
+# 示例
+# 2:
+#
+# 输入: k = 3, n = 9
+# 输出: [[1, 2, 6], [1, 3, 5], [2, 3, 4]]
+# 解释:
+# 1 + 2 + 6 = 9
+# 1 + 3 + 5 = 9
+# 2 + 3 + 4 = 9
+# 没有其他符合的组合了。
+# 示例
+# 3:
+#
+# 输入: k = 4, n = 1
+# 输出: []
+# 解释: 不存在有效的组合。
+# 在[1, 9]
+# 范围内使用4个不同的数字，我们可以得到的最小和是1 + 2 + 3 + 4 = 10，因为10 > 1，没有有效的组合。
+
+
+class Solution(object):
+    def combinationSum3(self, k, n):
+        """
+        :type k: int
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        path = []
+        res = []
+        _sum = 0
+        startIndex = 1
+        self.backtracking(k, n, _sum, path, res, startIndex)
+        return res
+
+    def backtracking(self, k, n, _sum, path, res, startIndex):
+        if _sum >= n and len(path) < k:
+            return
+
+        if _sum == n and len(path) == k:
+            res.append(path[:])
+            return
+
+        for i in range(startIndex, 10):
+            _sum += i
+            path.append(i)
+            self.backtracking(k, n, _sum, path, res, i + 1)
+            path.pop()
+            _sum -= i
